949ed36dc96eefbed98c08b925dd5fa571354a38
Vinod Shivoor <Vinod Shivoor>
2016-11-08 17:33:54 +0530

    donation changes

--- Donation and Pancard/fetchPancardDetails.js
+++ Donation and Pancard/fetchPancardDetails.js
@@ -12,6 +12,8 @@ var crmHeaders = {
 };
 
 function retreiveCurrentRecordPancard() {
+    window.parent.Xrm.Page.getAttribute("new_amount").setValue(null);
+    window.parent.Xrm.Page.getControl("new_subpledgeid").setFocus();
     configRecordID = configRecordID.replace(/[{}]/g, '');
     var odataSelect = window.parent.Xrm.Page.context.getClientUrl() + "/api/data/v8.0/accounts(" + configRecordID + ")";
     retrieveReq.open("GET", odataSelect, true);
@@ -28,32 +30,31 @@ function retreiveCurrentRecordPancard() {
             //pancardName = response['_new_pancard_value@OData.Community.Display.V1.FormattedValue'];
             pancardExists = response.new_pancardexist;
 
-           /* window.parent.Xrm.Page.getAttribute("new_pancard").setValue([{
-                id: pancardId,
-                name = pancardName,
-                entityType = pancardType,
-            }]);*/
+            /* window.parent.Xrm.Page.getAttribute("new_pancard").setValue([{
+                 id: pancardId,
+                 name = pancardName,
+                 entityType = pancardType,
+             }]);*/
             //window.parent.Xrm.Page.getAttribute("new_pancard").setValue(pancardId);
-         if(pancardExists==null)
-         {
-             window.parent.Xrm.Page.getAttribute("new_pancardexist").setValue(false)
-         }
-         else{
-            window.parent.Xrm.Page.getAttribute("new_pancardexist").setValue(pancardExists)
-         }
+            if (pancardExists == null) {
+                window.parent.Xrm.Page.getAttribute("new_pancardexist").setValue(false)
+            }
+            else {
+                window.parent.Xrm.Page.getAttribute("new_pancardexist").setValue(pancardExists)
+            }
         }
     };
     retrieveReq.send();
 }
 function Pancard() {
 
-    var lookupObj = window.parent.Xrm.Page.getAttribute("new_donororganization"); 
+    var lookupObj = window.parent.Xrm.Page.getAttribute("new_donororganization");
     if (lookupObj != null) {
         var lookupObjValue = lookupObj.getValue();
         if (lookupObjValue != null) {
-            configRecordID = lookupObjValue[0].id; 
+            configRecordID = lookupObjValue[0].id;
             retreiveCurrentRecordPancard();
-          
+
         }
     }
 }
