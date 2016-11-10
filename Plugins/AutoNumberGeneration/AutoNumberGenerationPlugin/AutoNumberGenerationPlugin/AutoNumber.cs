cef4e8c9b50ab8f0348f5254644f4197e8c266dd
Vinod Shivoor <Vinod Shivoor>
2016-11-10 11:48:15 +0530

    Plugin

---
+++ Plugins/AutoNumberGeneration/AutoNumberGenerationPlugin/AutoNumberGenerationPlugin/AutoNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+using Microsoft.Xrm.Sdk;
+
+namespace AutoNumberGenerationPlugin
+{
+    public class AutoNumber : IPlugin
+    {
+        IPluginExecutionContext context;
+        IOrganizationServiceFactory serviceFactory;
+        IOrganizationService service;
+        Entity entity, currentEntity;
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            service = serviceFactory.CreateOrganizationService(context.UserId);
+    
+        }
+
+    }
+}
